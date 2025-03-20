4dcc53792b5a90de03618ce2c0eca07705667534
A Nag <xanagx@github.com>
2025-03-19 19:20:21 -0700

    small edit

--- convert.py
+++ convert.py
@@ -1,13 +1,3 @@
-#
-#                Copyright (c) 1991-2025 Synopsys, Inc.
-#
-#   This software and the associated documentation are confidential and
-#   proprietary to Synopsys, Inc. Your use or disclosure of this software
-#   is subject to the terms and conditions of a written license agreement
-#   between you, or your company, and Synopsys, Inc.
-#
-
-
 import argparse
 from typing import Generator
 
